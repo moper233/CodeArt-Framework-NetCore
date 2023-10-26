a46cc4f6b33738dff0419558a6f259b92e34e5b7
moper233 <muyh748@vip.qq.com>
2023-10-26 16:11:00 +0800

    last

---
+++ CodeArt/CodeArt.Web/Procedure.cs
@@ -0,0 +1,40 @@
+﻿using CodeArt.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeArt.Web
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public abstract class Procedure : ControllerBase
+    {
+
+        //public virtual DTObject Invoke(DTObject arg)
+        //{
+        //    return InvokeDynamic(arg);
+        //}
+
+        protected virtual DTObject InvokeDynamic(dynamic arg)
+        {
+            return DTObject.Empty;
+        }
+
+        //[HttpGet]
+        //public string Get(string arg)
+        //{
+        //    var data = DTObject.Create(arg);
+        //    var result = this.InvokeDynamic(data);
+        //    return result.GetCode(false, false);
+        //}
+
+
+        [HttpPost]
+        public string Post(string arg)
+        {
+            var data = DTObject.Create(arg);
+            var result = InvokeDynamic(data);
+            return result.GetCode(false, false);
+        }
+    }
+
+
+}
